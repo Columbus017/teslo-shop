d07ebce93b489b05c648808e6c9a68cfdd41559d
Marlon Veliz <marlonveliz.nero19@gmail.com>
2025-08-13 20:15:41 -0600

    Prisma y SeedData

---
+++ src/seed/seed-database.ts
@@ -0,0 +1,63 @@
+import prisma from "../lib/prisma";
+import { initialData } from "./seed";
+
+async function main() {
+
+    // 1. Borrar registros previos
+    // await Promise.all([
+    //     prisma.productImage.deleteMany(),
+    //     prisma.product.deleteMany(),
+    //     prisma.category.deleteMany(),
+    // ]);
+
+    await prisma.productImage.deleteMany();
+    await prisma.product.deleteMany();
+    await prisma.category.deleteMany();
+
+    const { categories, products } = initialData
+
+    // Categorias
+    const categoriesData = categories.map(category => ({
+        name: category
+    }))
+    await prisma.category.createMany({ data: categoriesData });
+
+    const categoriesDB = await prisma.category.findMany();
+    
+    const categoriesMap = categoriesDB.reduce((map, category) => {
+        map[category.name.toLocaleLowerCase()] = category.id;
+        return map;
+    }, {} as Record<string, string>);
+
+    // Productos
+    products.forEach( async (product) => {
+        const { type, images, ...rest } = product;
+
+        const dbProduct = await prisma.product.create({
+            data: {
+                ...rest,
+                categoryID: categoriesMap[type]
+            }
+        })
+
+        // Insertar imagenes
+        const imagesData = images.map(image => ({
+            url: image,
+            productID: dbProduct.id
+        }));
+
+        await prisma.productImage.createMany({
+            data: imagesData
+        });
+    })
+    
+    
+    console.log('Seed ejecutado correctamente')
+}
+
+(() => {
+
+    if (process.env.NODE_ENV === 'production') return;
+
+    main();
+})();
--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -7,21 +7,22 @@ interface SeedProduct {
     slug: string;
     tags: string[];
     title: string;
-    type: ValidTypes;
+    type: 'shirts'|'pants'|'hoodies'|'hats';
     gender: 'men'|'women'|'kid'|'unisex'
 }
 
 type ValidSizes = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
-type ValidTypes = 'shirts'|'pants'|'hoodies'|'hats';
+type ValidTypes = 'Shirts'|'Pants'|'Hoodies'|'Hats';
 
 interface SeedData {
-    products: SeedProduct[],
+    categories: ValidTypes[];
+    products: SeedProduct[];
 }
 
-
-
-
 export const initialData: SeedData = {
+    categories: [
+        'Shirts', 'Pants', 'Hoodies', 'Hats'
+    ],
     products: [
         {
             description: "Introducing the Tesla Chill Collection. The Men’s Chill Crew Neck Sweatshirt has a premium, heavyweight exterior and soft fleece interior for comfort in any season. The sweatshirt features a subtle thermoplastic polyurethane T logo on the chest and a Tesla wordmark below the back collar. Made from 60% cotton and 40% recycled polyester.",
